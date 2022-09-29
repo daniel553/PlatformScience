a485f28ae88a59087614a38743b52a7653ecfeb0
Daniel García <daniel553@github.com>
2022-09-29 00:16:37 -0500

    [Added] new format for driver item view

--- app/src/main/java/com/tripletres/platformscience/ui/view/driver/DriverListScreen.kt
+++ app/src/main/java/com/tripletres/platformscience/ui/view/driver/DriverListScreen.kt
@@ -2,6 +2,7 @@ package com.tripletres.platformscience.ui.view.driver
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,7 +16,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -50,7 +54,7 @@ fun DriverListViewScreen(navController: NavController, viewModel: DriverListView
 @Composable
 fun DriverListView(drivers: List<DriverItem>, onDriverSelected: (driver: DriverItem) -> Unit) {
     LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = Modifier.padding(16.dp)
     ) {
         items(drivers) { driver ->
@@ -63,34 +67,60 @@ fun DriverListView(drivers: List<DriverItem>, onDriverSelected: (driver: DriverI
 
 @Composable
 fun DriverItemView(driver: DriverItem, onPressed: (id: Long) -> Unit) {
-    Box() {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .clickable { onPressed(driver.id) }
+    ) {
         Card(
+            shape = RoundedCornerShape(16.dp),
             backgroundColor = MaterialTheme.colors.background,
             elevation = 2.dp,
             modifier = Modifier
                 .fillMaxWidth()
                 .height(84.dp)
-                .padding(start = 40.dp, end = 16.dp)
+                .padding(start = 40.dp, end = 16.dp, top = 4.dp, bottom = 4.dp)
         ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.padding(start = 48.dp)
             ) {
-
-                Badge() {
-                    Text(text = driver.id.toString())
+                Column() {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "ID:",
+                            modifier = Modifier.padding(end = 4.dp)
+                        )
+                        Badge(
+                            backgroundColor = MaterialTheme.colors.secondaryVariant,
+                            contentColor = Color.White
+                        ) {
+                            Text(text = driver.id.toString())
+                        }
+                        Text(
+                            text = driver.name.uppercase(),
+                            style = MaterialTheme.typography.subtitle1,
+                            modifier = Modifier.padding(start = 8.dp)
+                        )
+                    }
+                    driver.shipmentItem?.let {
+                        Text(
+                            text = stringResource(id = R.string.driver_assigned_route)
+                                .plus(it.address.take(12).plus("...")),
+                            style = MaterialTheme.typography.caption,
+                            color = MaterialTheme.colors.secondaryVariant
+                        )
+                    }
                 }
-                Text(
-                    text = driver.name,
-                    modifier = Modifier.padding(start = 8.dp)
-                )
                 Spacer(modifier = Modifier.weight(weight = 1f, fill = true))
-                IconButton(
-                    onClick = { onPressed(driver.id) },
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowRight,
+                    contentDescription = "",
+                    tint = MaterialTheme.colors.primary,
                     modifier = Modifier.padding(start = 8.dp, end = 8.dp)
-                ) {
-                    Icon(imageVector = Icons.Default.KeyboardArrowRight, contentDescription = "")
-                }
+                )
             }
         }
 
@@ -113,7 +143,7 @@ fun DriverItemViewPreview() {
         DriverItem(
             10,
             "Pedro Daniel Garcia",
-            ShipmentItem(10, "Address"),
+            ShipmentItem(10, "Very long address 123"),
             1.5f
         )
     ) {}
