6814c0dd8a884f7ae3ead383694189b7771c910c
Daniel García <daniel553@github.com>
2022-10-01 13:12:22 -0500

    [Added] ACO unit test with error margin with larger input

--- app/src/test/java/com/tripletres/platformscience/domain/algorithm/AntColonyOptimizationAlgorithmUnitTest.kt
+++ app/src/test/java/com/tripletres/platformscience/domain/algorithm/AntColonyOptimizationAlgorithmUnitTest.kt
@@ -31,11 +31,11 @@ class AntColonyOptimizationAlgorithmUnitTest {
     }
 
     @Test
-    fun `Given assignation matrix When aco algorithm executed Then list of drivers assigned is return`() {
+    fun `Given larger assignation matrix When aco algorithm executed Then list of drivers assigned is return`() {
         //Given branching and bound algorithm with mock data
         val algorithm = AntColonyOptimizationAlgorithm()
-        val input = ssMockList()
-        val minSS = 13f
+        val input = ssMockList(major = true)
+        val minSSWithMarginError = 100f // Expected to be less than 8% as error margin (93)
 
         //When executing algorithm
         val matching = algorithm.getBestMatching(input = input)
@@ -49,6 +49,6 @@ class AntColonyOptimizationAlgorithmUnitTest {
         }
         var ss = 0f
         matching.map { it.ss }.toList().forEach { ss += it ?: 0f }
-        Assert.assertEquals(minSS, ss)
+        assert(minSSWithMarginError > ss)
     }
 }
